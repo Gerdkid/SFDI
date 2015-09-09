ff9326de0f9ae0a187f59522f34214a68f6a54dc
Gerdkid <joshgerding@gmail.com>
2015-09-09 13:52:37 -0400

    creating simple todo route

--- Rest-Api-Application/todoApp/app.js
+++ Rest-Api-Application/todoApp/app.js
@@ -6,9 +6,11 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
+
+var todos = require('./routes/todos');
+app.use('/todos', todos);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,7 +25,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -54,6 +55,15 @@ app.use(function(err, req, res, next) {
     message: err.message,
     error: {}
   });
+});
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/todoApp', function(err) {
+    if(err) {
+        console.log('connection error', err);
+    } else {
+        console.log('connection successful');
+    }
 });
 
 
