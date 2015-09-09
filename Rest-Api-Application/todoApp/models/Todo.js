56e5a4790ba27f66783b855fa611644de43982ae
Gerdkid <joshgerding@gmail.com>
2015-09-09 14:16:56 -0400

    setting up rest api

--- Rest-Api-Application/todoApp/models/Todo.js
+++ Rest-Api-Application/todoApp/models/Todo.js
@@ -4,7 +4,6 @@ var TodoSchema = new mongoose.Schema({
   name: String,
   completed: Boolean,
   note: String,
-  updated_at: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model('Todo', TodoSchema);
