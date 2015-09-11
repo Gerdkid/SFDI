6760ef603cef95b8471aa8e0f0df23880150c110
Gerdkid <joshgerding@gmail.com>
2015-09-11 00:38:17 -0400

    Cleaning up code

--- Rest-API/models/Courses.js
+++ Rest-API/models/Courses.js
@@ -3,7 +3,6 @@ var mongoose = require('mongoose');
 var CourseSchema = new mongoose.Schema({
   CourseName: String,
   Year: String,
-	ImageUrl: String,
   note: String,
   updated_at: { type: Date, default: Date.now },
 
--- Rest-API/routes/index.js
+++ Rest-API/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'REST API Backend App' });
+});
+
+router.get('/courseslist', function(req, res) {
+  res.render('courses', { title: 'Courses' });
 });
 
 module.exports = router;
