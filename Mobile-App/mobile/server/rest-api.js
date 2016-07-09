3d5eb96b242a922b0d2b419237cab2a75710b3f8
Gerdkid <joshgerding@gmail.com>
2016-07-09 00:04:52 -0400

    Fixing indents

--- Mobile-App/mobile/server/rest-api.js
+++ Mobile-App/mobile/server/rest-api.js
@@ -1,25 +1,25 @@
 //REST API GET Calls for the app
 
 Meteor.methods({
- refresh: function () {
+  refresh: function() {
 
- Meteor.http.get("http://localhost:5000/todos",function(err,result){
- if(!err){
-	 Todolist.remove({});
-	 var TodolistData = result.data;
-	 TodolistData.forEach( function (todo){
-		 Todolist.insert(todo);
-		 });
-	 }
-});
-Meteor.http.get("http://localhost:5000/courses",function(err,result){
-if(!err){
-  Courses.remove({});
-  var courseData = result.data;
-  courseData.forEach( function (course){
-    Courses.insert(course);
+    Meteor.http.get("http://localhost:5000/todos", function(err, result) {
+      if (!err) {
+        Todolist.remove({});
+        var TodolistData = result.data;
+        TodolistData.forEach(function(todo) {
+          Todolist.insert(todo);
+        });
+      }
+    });
+    Meteor.http.get("http://localhost:5000/courses", function(err, result) {
+      if (!err) {
+        Courses.remove({});
+        var courseData = result.data;
+        courseData.forEach(function(course) {
+          Courses.insert(course);
+        });
+      }
     });
   }
-});
-}
 });
